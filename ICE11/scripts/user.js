cf355f355a49452704b79a2ecdf4a26e66b2cba4
Neema Mwansembo <neyharry2003@gmail.com>
2023-04-02 22:18:48 -0400

    ICE11

--- ICE11/scripts/user.js
+++ ICE11/scripts/user.js
@@ -1,54 +1,66 @@
-(function(core) {
-
+"use strict";
+var core;
+(function (core) {
     class User {
-        
-        // Constructor
         constructor(displayName = "", emailAddress = "", username = "", password = "") {
-            this.DisplayName = displayName
-            this.EmailAddress = emailAddress
-            this.Username = username
-            this.Password = password
+            this.m_displayName = displayName;
+            this.m_emailAddress = emailAddress;
+            this.m_username = username;
+            this.m_password = password;
         }
-
-        // TODO: input issing Getters and Setters eventually
-
-        // Override Method
+        get DisplayName() {
+            return this.m_displayName;
+        }
+        set DisplayName(name) {
+            this.m_displayName = name;
+        }
+        get EmailAddress() {
+            return this.m_emailAddress;
+        }
+        set EmailAddress(emailAddress) {
+            this.m_emailAddress = emailAddress;
+        }
+        get Username() {
+            return this.m_username;
+        }
+        set Username(username) {
+            this.m_username = username;
+        }
+        get Password() {
+            return this.m_password;
+        }
+        set Password(password) {
+            this.m_password = password;
+        }
         toString() {
-            return `Display Name: ${ this.DisplayName }\nEmail Address: ${ this.EmailAddress }\nUsername: ${ this.Username }`
+            return `Display Name: ${this.DisplayName}\nEmail Address: ${this.EmailAddress}\nUsername: ${this.Username}`;
         }
-
-        // Utility Methods
         toJSON() {
             return {
                 "DisplayName": this.DisplayName,
                 "EmailAddress": this.EmailAddress,
                 "Username": this.Username
-            }
+            };
         }
         fromJSON(data) {
-            this.DisplayName = data.DisplayName
-            this.EmailAddress = data.EmailAddress
-            this.Username = data.Username
-            this.Password = data.Password
+            this.DisplayName = data.DisplayName;
+            this.EmailAddress = data.EmailAddress;
+            this.Username = data.Username;
+            this.Password = data.Password;
         }
-
-        // Serialize and Deserialize
-        // Serialize Method
         serialize() {
             if (this.DisplayName !== "" && this.EmailAddress !== "" && this.Username !== "")
-                return `${ this.DisplayName }, ${ this.EmailAddress }, ${ this.Username }`
-            console.error("One or more properties or fields of the Contact Object are missing or invalid!")
-            return null
+                return `${this.DisplayName}, ${this.EmailAddress}, ${this.Username}`;
+            console.error("One or more properties or fields of the Contact Object are missing or invalid!");
+            return null;
         }
-    
-        // Deserialize Method
         deserialize(data) {
-            let propertyArray = data.split(",")
-            this.DisplayName = propertyArray[0]
-            this.EmailAddress = propertyArray[1]
-            this.Username = propertyArray[2]
+            let propertyArray = data.split(",");
+            this.DisplayName = propertyArray[0];
+            this.EmailAddress = propertyArray[1];
+            this.Username = propertyArray[2];
         }
     }
-
-    core.User = User
-})(core || (core={}))
+    core.User = User;
+})(core || (core = {}));
+//# sourceMappingURL=user.js.map
